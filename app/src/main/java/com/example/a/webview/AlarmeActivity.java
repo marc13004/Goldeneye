ded9d1101a7bcbe2da33f0cf557d5286aac67ee8
34011-82-01 <audriclespa@free.fr>
2016-12-13 09:57:06 +0100

    first one

--- app/src/main/java/com/example/a/webview/AlarmeActivity.java
+++ app/src/main/java/com/example/a/webview/AlarmeActivity.java
@@ -9,5 +9,6 @@ public class AlarmeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarme);
+        11111111111111111
     }
 }
