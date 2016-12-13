edebd64063ec6cc76dc8aaef88d74edb5c6d7ca5
34011-82-01 <audriclespa@free.fr>
2016-12-13 12:10:04 +0100

    cameraactivity

--- app/src/main/java/com/example/a/webview/CameraActivity.java
+++ app/src/main/java/com/example/a/webview/CameraActivity.java
@@ -1,5 +1,6 @@
 package com.example.a.webview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -10,19 +11,32 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        final WebView webview=(WebView) findViewById(R.id.webview);
-        int default_zoom_level =100;
-        webview.setInitialScale(default_zoom_level);
-        webview.post(new Runnable()
-        {
 
-            @Override
-            public void run() {
-                int width = webview.getWidth();
-                int height = webview.getHeight();
-                webview.loadUrl("http://10.111.61.96:8090/");
-            }
-        });
+        boolean stream = false;
+
+        String URL = "http://10.111.61.96:8081/streaming";
+        WebServiceGET WebServiceGET = new WebServiceGET();
+        WebServiceGET.execute(URL);
+
+        if (WebServiceGET.httpStatus == 200) {
+            stream = true;
+            final WebView webview = (WebView) findViewById(R.id.webview);
+            int default_zoom_level = 100;
+            webview.setInitialScale(default_zoom_level);
+            webview.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    int width = webview.getWidth();
+                    int height = webview.getHeight();
+                    webview.loadUrl("http://10.111.61.96:8090/");
+                }
+            });
+        }
+        else {
+            Intent activityChangeIntent = new Intent(CameraActivity.this, VuePrincipaleActivity.class);
+            CameraActivity.this.startActivity(activityChangeIntent);
+        }
 
     }
 
